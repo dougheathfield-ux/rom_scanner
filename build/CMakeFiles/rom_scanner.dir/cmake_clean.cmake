ed437d7455e2e54f178c2c7e19640cf21cc424c9
Doug <dougheathfield@googlmail.com>
2026-09-21 05:28:10 +0100

    TESTING COMMIT

---
+++ build/CMakeFiles/rom_scanner.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rom_scanner.dir/_deps/imgui-src/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/rom_scanner.dir/_deps/imgui-src/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/rom_scanner.dir/_deps/imgui-src/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/rom_scanner.dir/_deps/imgui-src/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/rom_scanner.dir/_deps/imgui-src/imgui.cpp.o"
+  "CMakeFiles/rom_scanner.dir/_deps/imgui-src/imgui.cpp.o.d"
+  "CMakeFiles/rom_scanner.dir/_deps/imgui-src/imgui_draw.cpp.o"
+  "CMakeFiles/rom_scanner.dir/_deps/imgui-src/imgui_draw.cpp.o.d"
+  "CMakeFiles/rom_scanner.dir/_deps/imgui-src/imgui_tables.cpp.o"
+  "CMakeFiles/rom_scanner.dir/_deps/imgui-src/imgui_tables.cpp.o.d"
+  "CMakeFiles/rom_scanner.dir/_deps/imgui-src/imgui_widgets.cpp.o"
+  "CMakeFiles/rom_scanner.dir/_deps/imgui-src/imgui_widgets.cpp.o.d"
+  "CMakeFiles/rom_scanner.dir/src/main.cpp.o"
+  "CMakeFiles/rom_scanner.dir/src/main.cpp.o.d"
+  "rom_scanner"
+  "rom_scanner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rom_scanner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/rom_scanner.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for rom_scanner.
+# This may be replaced when dependencies are built.
